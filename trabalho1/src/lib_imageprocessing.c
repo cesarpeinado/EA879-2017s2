ff4ccbdaf51ae3c46106452f838ccc09cb6cbd63
brendabraga <brendabraga>
2017-09-12 08:54:12 -0300

    Funciona pixel maximo

--- trabalho1/src/lib_imageprocessing.c
+++ trabalho1/src/lib_imageprocessing.c
@@ -115,28 +115,18 @@ void brilho (imagem I, float intensidade, char ope, char *nome_do_arquivo){
    }
 }
 
-int busca (imagem I){
+void busca (imagem I){
    float x, y;
    int i, j, max=0, soma;
 
    x = I.width;
    y = I.height;
 
-   for( j = 0; j < x*y; j++){
-	   soma = I.r[j] + I.g[j] + I.b[j];
+   for( j = 0; j < 5038848; j++){
+	   soma = I.r[j] + I.g[j] + I.b[j]; //printf("ENTREI\n");
 		if (soma > max){
 		   max = soma;
 		}
    }
-return max;
+	printf("Pixel maximo: %d\n", max);
 }
-
-
-
-
-
-
-
-
-
-
